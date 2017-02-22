e08e2d0d8c0be28ae2d6e9591138640b51555277
Nate <nli12@umd.edu>
2017-02-22 00:19:57 -0500

    test

--- routes/index.js
+++ routes/index.js
@@ -98,7 +98,8 @@ router.post('/log', function(req,res){
 		var userAccount = database.collection('UserAccount');
 		userAccount.findOne({auth_name: req.body.authName}, function(err, doc) {
 			if (err) throw err;
-			console.log(JSON.stringify(doc[messages]))
+			console.log(doc);
+			console.log(JSON.stringify(doc[messages]));
 			fs.writeFile('test.txt', JSON.stringify(doc[messages]), function(error) {
 				res.download('test.txt');
 			}); 
